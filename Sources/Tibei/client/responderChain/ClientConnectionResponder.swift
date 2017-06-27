b8cf3fc5448dcd2ca9ee63dc239e71be29cbd975
Valbrand <daniel@dvalbrand.com>
2017-06-26 21:21:19 -0300

    Added HeaderDocs for ClientConnectionResponder

--- Sources/Tibei/client/responderChain/ClientConnectionResponder.swift
+++ Sources/Tibei/client/responderChain/ClientConnectionResponder.swift
@@ -8,11 +8,18 @@
 
 import Foundation
 
+/**
+ A specialization of `ConnectionResponder` with additional behavior for the client side.
+ */
 public protocol ClientConnectionResponder: ConnectionResponder {
+    /** 
+     If the `ClientMessenger` instance is browsing for services, this method gets called whenever a new service becomes available (or if an available service goes offline), receiving the updated service list as a parameter.
+    */
     func availableServicesChanged(availableServiceIDs: [String])
 }
 
 public extension ClientConnectionResponder {
+    /// Does nothing
     func availableServicesChanged(availableServiceIDs: [String]) {
     }
 }
